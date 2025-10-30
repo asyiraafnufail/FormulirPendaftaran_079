9fed3aa141be4734702f6f25a63097cd23195ebf
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-30 15:41:58 +0700

    Menambahkan variabel untuk input

--- app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
+++ app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
@@ -21,5 +23,12 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+fun FormulirPendaftaran() {
+    // variabel input
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
 
 
