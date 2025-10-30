278e3fe2758df9385969037fc978c570b71fd94e
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-30 15:39:46 +0700

    Menambahkan Surface utama dan memanggil FormulirPendaftaran()

--- app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
+++ app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
@@ -3,9 +3,19 @@ package com.example.formulirpendaftaran
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color(0xFFEDE7F6)
+            ) {
+                FormulirPendaftaran()
+            }
+        }
 }
