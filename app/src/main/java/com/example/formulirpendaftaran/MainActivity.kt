d6e7447174130ffd216e79600c3eecbfbd775893
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-30 15:41:04 +0700

    Menutup struktur class main activity

--- app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
+++ app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.formulirpendaftaran
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,4 +19,7 @@ class MainActivity : ComponentActivity() {
                 FormulirPendaftaran()
             }
         }
+    }
 }
+
+
