c85d8f971b5f980a7beb2ff8e980bb66355abb8e
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-30 15:37:58 +0700

    Menambahkan fungsi onCreate untuk inisialisasi tampilan Compose

--- app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
+++ app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.formulirpendaftaran
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+}
